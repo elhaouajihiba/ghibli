8885a48512aaf13af0a22d3a1a7da2169de52183
SinemORUC <sinem.f.oruc@gmail.com>
2019-11-18 13:11:00 +0100

    derniere version Sinem Vanessa

--- index.js
+++ index.js
@@ -11,13 +11,83 @@ app.engine('hbs', hbs({
 }));
 app.set('view engine', 'hbs');
 
-app.get('/film', function (req, res) {
-  res.render("Chihiro.hbs", {
+app.get('/Miyazaki', function (req, res) {
+  res.render("ModelePage.hbs", {
     illustration : {
       path: "photos/Chihiro6.jpeg"
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"]
     }
   });
 })
+
+app.get('/Chihiro', function (req, res) {
+  res.render("ModelePage.hbs", {
+    illustration : {
+      path: "photos/Chihiro6.jpeg"
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"]
+    }
+  });
+})
+
+app.get('/Chateau', function (req, res) {
+  res.render("ModelePage.hbs", {
+    illustration : {
+      path: "photos/Chihiro6.jpeg"
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"]
+    }
+  });
+})
+
+app.get('/Totoro', function (req, res) {
+  res.render("ModelePage.hbs", {
+    illustration : {
+      path: "photos/Chihiro6.jpeg"
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"]
+    }
+  });
+})
+
+app.get('/Mononoke', function (req, res) {
+  res.render("ModelePage.hbs", {
+    illustration : {
+      path: "photos/Chihiro6.jpeg"
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"]
+    }
+  });
+})
+
+app.get('/Ponyo', function (req, res) {
+  res.render("ModelePage.hbs", {
+    illustration : {
+      path: "photos/Chihiro6.jpeg"
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"]
+    }
+  });
+})
+
+app.get('/Terremer', function (req, res) {
+  res.render("ModelePage.hbs", {
+    illustration : {
+      path: "photos/Chihiro6.jpeg"
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"]
+    }
+  });
+})
+
 
 app.get('/*', function (req, res) {
   res.render("PageWeb.hbs", {});
