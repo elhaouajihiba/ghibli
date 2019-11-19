156d24d4da04728bcf811f7224bc9d2faebdd2cd
SinemORUC <sinem.f.oruc@gmail.com>
2019-11-19 12:47:46 +0100

    page films Sinem Vanessa

---
+++ movies.js
@@ -0,0 +1,76 @@
+let movies = {
+  "Totoro" : {
+    illustration : {
+      title : "Mon voisin Totoro",
+      path: "/photos/Totoro2.jpeg",
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"],
+      images : ["image1", "image2"]
+    }
+
+  },
+
+  "Chihiro" : {
+    illustration : {
+      title : "Chihiro",
+      path: "/photos/Chihiro6.jpeg",
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"],
+      images : ["image1", "image2"]
+    }
+
+  },
+
+
+  "Mononoke" : {
+    illustration : {
+      title : "Princesse Mononoke",
+      path: "/photos/Mononoke.jpg",
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"],
+      images : ["image1", "image2"]
+    }
+
+  },
+
+  "Chateau" : {
+    illustration : {
+      title : "Le Chateau Ambulant",
+      path: "/photos/Chateau.jpg",
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"],
+      images : ["image1", "image2"]
+    }
+
+  },
+
+  "Ponyo" : {
+    illustration : {
+      title : "Ponyo sur la Falaise",
+      path: "/photos/Ponyo.jpg",
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"],
+      images : ["image1", "image2"]
+    }
+
+  },
+
+  "Terremer" : {
+    illustration : {
+      title : "Les contes de Terremer",
+      path: "/photos/Terremer.jpeg",
+    },
+    content : {
+      text : ["paragraphe1", "paragraphe2"],
+      images : ["image1", "image2"]
+    }
+}
+}
+
+
+module.exports=movies;
