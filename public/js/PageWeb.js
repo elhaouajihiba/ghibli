7d25f1f433356bba7f2a8e501d1fb0389304f4c1
elhaouajihiba <elhaouajihiba@gmail.com>
2019-12-25 18:27:54 +0100

    nouvelle version

--- public/js/PageWeb.js
+++ public/js/PageWeb.js
@@ -8,3 +8,27 @@ $(window).scroll(function() {
     }
   });
 });
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "#EED9C4";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "#EED9C4";
+}
+
+
+/*$(window).load(function(){
+  var scrollTop= $(this).scrollTop();
+  $('.background').css({
+    opacity: function() {
+      var elementHeight = $(this).height();
+      return 0.09-(scrollTop - elementHeight) /elementHeight ;
+    }
+});
+});
+*/
