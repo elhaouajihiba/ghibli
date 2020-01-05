bfada80d3d79a9b6bdf6c3055ea0e01149386162
Vanessa <vmohammedi@outlook.fr>
2020-01-05 15:36:32 +0100

    corrections

--- public/js/PageWeb.js
+++ public/js/PageWeb.js
@@ -1,24 +1,34 @@
-
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("menu").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "#EED9C4";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("menu").style.marginLeft= "0";
-  document.body.style.backgroundColor = "#EED9C4";
-}
-
-
 $(window).scroll(function() {
   var scrollTop= $(this).scrollTop();
 
   $('.ban').css({
     opacity: function() {
-      return 0.09-(scrollTop - window.innerHeight ) /window.innerHeight;
+      var elementHeight = $(this).height();
+      return 0.09-(scrollTop - elementHeight) /elementHeight ;
     }
   });
 });
-AOS.init();
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "#EED9C4";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "#EED9C4";
+}
+
+
+/*$(window).load(function(){
+  var scrollTop= $(this).scrollTop();
+  $('.background').css({
+    opacity: function() {
+      var elementHeight = $(this).height();
+      return 0.09-(scrollTop - elementHeight) /elementHeight ;
+    }
+});
+});
+*/
