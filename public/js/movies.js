3d148ba1b10e17177b5c6a1193325420b927bfc1
elhaouajihiba <elhaouajihiba@gmail.com>
2020-01-16 01:59:17 +0100

    GOOD JOB!

--- public/js/movies.js
+++ public/js/movies.js
@@ -2,7 +2,7 @@ let movies = {
   "Totoro" : {
     illustration : {
       title : "Mon voisin Totoro",
-      path: "/photos/totoro.jpg",
+      path: "/photos/totoro1.jpg",
     },
     content1 : {
       text : `Deux petites filles viennent s'installer avec leur père dans une grande maison à la campagne afin de se rapprocher de l'hôpital ou séjourne leur mère. Elles vont découvrir l'existence de créatures merveilleuses, mais très discrètes, les totoros.
@@ -15,7 +15,7 @@ let movies = {
       images :  "voir/photos/ghibli.png",
       titre : `A savoir`,
     },
-    
+
     video : {
       srcVideo : "https://www.youtube.com/embed/jD-6_0h4KwM"
     }
@@ -30,15 +30,15 @@ let movies = {
       text : `Chihiro, dix ans, a tout d'une petite fille capricieuse. Elle s'apprête à emménager avec ses parents dans une nouvelle demeure.
       Sur la route, la petite famille se retrouve face à un immense bâtiment rouge au centre duquel s'ouvre un long tunnel. De l'autre côté du passage se dresse une ville fantôme. Les parents découvrent dans un restaurant désert de nombreux mets succulents et ne tardent pas à se jeter dessus. Ils se retrouvent alors transformés en cochons.
       Prise de panique, Chihiro s'enfuit et se dématérialise progressivement. L'énigmatique Haku se charge de lui expliquer le fonctionnement de l'univers dans lequel elle vient de pénétrer. Pour sauver ses parents, la fillette va devoir faire face à la terrible sorcière Yubaba, qui arbore les traits d'une harpie méphistophélique.`,
-      images : "/photos/chihiro8.jpg",
+      images : "/photos/wp10bis.jpeg",
       titre : `Resumé`,
     },
     content2 : {
       text : `À l'époque où le film est réalisé, Hayao Miyazaki passe toutes ses vacances d'été dans un chalet de montagne avec sa famille et cinq petites filles d'amis de la famille. L'idée du Voyage de Chihiro lui vient en voulant faire un film pour ces fillettes. Il a déjà réalisé des œuvres telles que Mon voisin Totoro et Kiki la petite sorcière, principalement destinées aux enfants et adolescents, mais n'a jamais créé un film s'adressant spécifiquement aux préadolescentes. Cherchant l'inspiration, il lit des magazines de prépublication de manga shōjo tels que Nakayoshi et Ribon laissés par les filles dans le chalet, mais est déçu de ne trouver pour sujets que des amourettes ou des romances. Pensant que ces mangas ne montrent pas ce que les jeunes filles « [ont] de plus cher dans leurs cœurs », Miyazaki décide de réaliser un film sur une héroïne qui serait une petite fille ordinaire, à laquelle il serait possible de s'identifier.`,
-      images : "/photos/chihiro8.jpg",
+      images : "/photos/chihiro2.jpg",
       titre : `A savoir`,
     },
-    
+
     video : {
       srcVideo : "https://www.youtube.com/embed/5-cro68n7CE"
     }
@@ -60,10 +60,10 @@ let movies = {
       text : `Dame Eboshi et les villageois repoussent une attaque des samouraïs du seigneur Asano, qui cherche à s'emparer du village et des armes qui y sont produites. Dame Eboshi rencontre ensuite Jiko, qui travaille en réalité pour l'empereur et a été chargé de lui ramener la tête du dieu-cerf, censée accorder l'immortalité. Tous deux décident de s'allier dans cette tâche.
 
       lage des forges d'où il pourra souvent aller voir San. Dame Eboshi, en reconnaissance de leurs actions, s'engage à repartir de zéro pour que son village cohabite plus en harmonie avec la forêt, tandis que Jiko, fataliste, pense à prendre sa retraite. `,
-      images : "/photos/mononoke3.jpg",
+      images: "/photos/mononoke3.jpg",
       titre : `A savoir`,
     },
-    
+
     video : {
       srcVideo : "https://www.youtube.com/embed/4OiMOHRDs14"
     }
@@ -73,7 +73,7 @@ let movies = {
   "Chateau" : {
     illustration : {
       title : "Le Château Ambulant",
-      path: "/photos/chateau1.jpg",
+      path: "/photos/cha2.jpg",
     },
     content1 : {
       text : `La jeune Sophie, âgée de 18 ans, travaille sans relâche dans la boutique de chapelier que tenait son père avant de mourir. Lors de l'une de ses rares sorties en ville, elle fait la connaissance de Hauru le Magicien. Celui-ci est extrêmement séduisant, mais n'a pas beaucoup de caractère... Se méprenant sur leur relation, une sorcière jette un épouvantable sort sur Sophie et la transforme en vieille femme de 90 ans.
@@ -86,9 +86,9 @@ let movies = {
       images :  "/photos/chateau3.jpg",
       titre : `A savoir`,
     },
-    
+
     video : {
-      srcVideo : "https://www.youtube.com/embed/pGEsAlY49hw" 
+      srcVideo : "https://www.youtube.com/embed/pGEsAlY49hw"
     }
 
   },
@@ -99,7 +99,7 @@ let movies = {
       path: "/photos/Ponyo.jpg",
     },
     content1 : {
-      text : `Le petit Sosuke, cinq ans, habite un village construit au sommet d'une falaise qui surplombe la Mer Intérieure. Un beau matin, alors qu'il joue sur la plage en contrebas, il découvre une petite fille poisson rouge nommée Ponyo, piégée dans un pot de confiture. Sosuke la sauve, et décide de la garder avec lui dans un seau. 
+      text : `Le petit Sosuke, cinq ans, habite un village construit au sommet d'une falaise qui surplombe la Mer Intérieure. Un beau matin, alors qu'il joue sur la plage en contrebas, il découvre une petite fille poisson rouge nommée Ponyo, piégée dans un pot de confiture. Sosuke la sauve, et décide de la garder avec lui dans un seau.
       Le petit garçon lui promet de la protéger et de s'occuper d'elle, mais le père de Ponyo, Fujimoto - un sorcier autrefois humain qui vit tout au fond de la mer - la force à revenir avec lui dans les profondeurs. Bien décidée à devenir humaine, Ponyo s'échappe pour retrouver Sosuke.
       Mais avant de prendre la fuite, elle répand l'élixir magique de Fujimoto, l'Eau de la Vie, dans l'océan. Le niveau de la mer s'élève, et les soeurs de Ponyo sont transformées en vagues gigantesques qui montent jusqu'à la maison de Sosuke sur la falaise, et engloutissent le village...`,
       images : "/photos/ponyo3.jpg",
@@ -109,16 +109,16 @@ let movies = {
       text : `Alors que le sorcier sort, Ponyo est libérée par ses sœurs de la bulle où elle était maintenue prisonnière, et fait repousser ses membres. Décidant de partir à la recherche de son ami, elle submerge le repaire de Fujimoto et libère l’énergie du puits d’eau de vie, ce qui lui permet d’achever sa transformation en petite fille. Chevauchant un banc de poissons, elle jaillit à la surface, provoquant un typhon, qui manque de faire chavirer le Koganei Maru. Au Tournesols, Sôsuké dit au revoir à sa maîtresse et rejoint sa mère à l’hospice, où il offre des origamis aux vieilles dames. Lisa l’emmène en voiture, et ils partent sous la pluie battante, mais le trajet se transforme en une course contre la mer démontée, sur laquelle le garçonnet voit une petite fille courir. Alors qu’ils se garent chez eux, Ponyo surgit des vagues et se jette dans les bras de Sôsuké ; celui-ci ne reconnaît pas son amie au premier abord, mais finit par comprendre qui elle est. Lisa coupe court aux retrouvailles pour emmener les deux enfants à l’abri dans la maison. Après les avoir séchés et nourris, la jeune femme, qui s’inquiète pour les occupants des Tournesols, profite d’une accalmie provoquée par l’assoupissement de Ponyo pour retourner à l’hospice, laissant Sôsuké garder la maison.
 
       Au large, durant la nuit, le Koganei Maru est en perdition, les marins voyant au loin les lumières de tous les autres navires en panne également ; soudain, Gran Mamare, divinité de la mer et mère de Brunehilde, passe à la surface de l’eau, et les turbines se remettent en route. Fujimoto tente de s’approcher de la maison où Sôsuké et Ponyo dorment, mais est repoussé par un sortilège d’exclusion posé par sa fille, qu’il ne peut qu’apercevoir par la fenêtre ; la divinité de la mer arrive, et s’entretient avec le sorcier. Celui-ci est terrifié par les événements provoqués par Brunehilde, devenue humaine : la mer est revenue à l’époque du Dévonien (deux Dunkleosteus apparaissent dans un court passage), la lune se rapproche de la Terre, et les satellites artificiels commencent à tomber, pour se consumer dans l’atmosphère, mais Gran Mamare le rassure en lui rappelant la magie ancienne selon laquelle une sirène peut perdre ses pouvoirs et devenir une femme, si le cœur de l’homme qu’elle aime ne faiblit pas ; dans le cas contraire, elle est transformée en écume.
-      
+
       Le lendemain, les deux enfants se réveillent ; le niveau de la mer, qui foisonne de poissons disparus depuis des centaines de millions d’années, est monté jusqu’au seuil de la maison. Avec ses pouvoirs, Ponyo élargit le bateau jouet de Sôsuké, et les deux enfants montent à bord avec des provisions ; mettant en route le moteur pop-pop, ils suivent la route submergée. Croisant un couple avec leur bébé sur une barque, ils leur offrent de la soupe et des sandwichs et récupèrent une bougie de rechange ; ils voient ensuite tous les gens du village passer sur une file de bateaux à rames, mais personne n’ayant vu Lisa, ils poursuivent leur route. Lorsque la bougie initiale finit de se consumer, Sôsuké demande à Ponyo d’agrandir la nouvelle, mais la petite fille tombe de sommeil ; le garçonnet se met donc à l’eau pour pousser le bateau à la nage. Ils finissent par arriver à un endroit où la route émerge, alors que le bateau jouet reprend sa taille normale, et le garçon affolé retrouve la voiture de Lisa sans son occupante. Le prenant par la main, son amie lui redonne du courage, et ils poursuivent leur route à pied.
-      
+
       Aux Tournesols, les habitations submergées sont protégées par une énorme bulle, et Lisa discute en privé avec Gran Mamare. Fujimoto rassemble les vieilles dames, qui ont retrouvé l’usage de leurs jambes, pour les prévenir que Sôsuké et Ponyo sont en route et qu’elles vont être les témoins de leur épreuve. Suivant la route, les deux enfants arrivent à l’entrée d’un tunnel, et alors qu’ils le traversent, la petite fille reprend sa forme de poisson, ne donnant plus signe de vie. Le garçonnet, affolé, la remet dans son seau et tente de lui faire reprendre conscience, lorsque Fujimoto apparaît et lui dit de la laisser dormir et de l’accompagner pour retrouver Lisa et les vieilles dames. Alors que Sôsuké ne lui fait pas confiance, et tente de rejoindre la vieille Toki qui n’a pas voulu le croire non plus, le sorcier envoie ses esprits marins pour les submerger, et les convoie jusqu’aux Tournesols, où le petit garçon s’entretient avec Gran Mamare. Alors qu’il affirme accepter son amie telle qu’elle est et l’aimer quelle que soit sa forme, et que cette dernière accepte de renoncer à ses pouvoirs pour grandir et vivre auprès de son ami, les réactions en chaîne cataclysmiques s’interrompent, et tout le monde est ramené à la surface. Tandis que Fujimoto s’excuse et serre la main de Sôsuké, Ponyo saute du seau et embrasse son ami, reprenant sa forme de petite fille ; les bateaux des marins, dont celui de Kôichi, et les barques des gens du village accostent, et tout le monde se retrouve. `,
       images : "/photos/ponyo2.jpg",
       titre : `A savoir`,
     },
 
     video : {
-      srcVideo : "https://www.youtube.com/embed/YTrEECZhpL0" 
+      srcVideo : "https://www.youtube.com/embed/YTrEECZhpL0"
     }
   },
 
@@ -137,7 +137,7 @@ let movies = {
       images : "/photos/ursula.jpg",
       titre : `A savoir`,
     },
-    
+
     video : {
       srcVideo : "https://www.youtube.com/embed/-T1iFyyL42M"
     }
